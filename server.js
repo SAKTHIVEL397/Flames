e9468a645c5385e0cae23ea7dba67d4ec7258624
unknown <you@exampleps5585058@gmail.com>
2022-01-26 13:24:19 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Flames'));
+app.use(express.static('/new/flames/src'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Flames/'}),
+    res.sendFile('index.html', {root: '/new/flames/src'}),
 );
 
 // Start the app by listening on the default Heroku port
